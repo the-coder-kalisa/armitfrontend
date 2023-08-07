60793aa8bd5207457e8073f9d8bfdbbb280919e3
unknown <dan38003@gmail.com>
2023-08-07 11:50:46 +0200

    change base url

--- src/store/constant.js
+++ src/store/constant.js
@@ -3,5 +3,5 @@ export const gridSpacing = 3
 export const drawerWidth = 260
 export const appDrawerWidth = 320
 export const maxScroll = 100000
-export const baseURL = 'http://3.135.164.73:3010'
+export const baseURL = 'https://langchain.agentsmith.live'
 export const uiBaseURL = window.location.origin
